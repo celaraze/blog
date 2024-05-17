8628a86043a31417824cb84382f7017e5f3a5878
celaraze <celaraze@qq.com>
2024-05-17 17:51:31 +0800

    测试不熟

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://celaraze.com',
+    base: 'blog',
     integrations: [
         starlight({
             title: 'Celaraze Blog',

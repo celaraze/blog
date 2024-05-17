01677405ae4cab0c00e4c6d4e47db2a598b08bdc
celaraze <celaraze@qq.com>
2024-05-18 01:58:34 +0800

    测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,54 @@
-import {defineConfig} from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://celaraze.com',
-    // base: 'blog',
-    integrations: [
-        starlight({
-            title: 'Celaraze Blog',
-            social: {
-                github: 'https://github.com/celaraze',
-            },
-            sidebar: [
-                {
-                    label: '首页',
-                    link: '/首页',
-                },
-                {
-                    label: '运维',
-                    autogenerate: {directory: '运维'},
-                    collapsed: true,
-                },
-                {
-                    label: '网络安全',
-                    autogenerate: {directory: '网络安全'},
-                    collapsed: true,
-                },
-                {
-                    label: '开发',
-                    autogenerate: {directory: '开发'},
-                    collapsed: true,
-                },
-                {
-                    label: '容器化',
-                    autogenerate: {directory: '容器化'},
-                    collapsed: true,
-                },
-                {
-                    label: '小作文',
-                    autogenerate: {directory: '小作文'},
-                    collapsed: true,
-                },
-            ],
-            locales: {
-                root: {
-                    label: '简体中文',
-                    lang: 'zh-CN',
-                },
-            },
-        }),
-    ],
+  site: "https://celaraze.com",
+  // base: 'blog',
+  integrations: [
+    starlight({
+      title: "Celaraze Blog",
+      social: {
+        github: "https://github.com/celaraze",
+      },
+      sidebar: [
+        {
+          label: "首页",
+          link: "/首页",
+        },
+        {
+          label: "运维",
+          autogenerate: { directory: "运维" },
+          collapsed: true,
+        },
+        {
+          label: "网络安全",
+          autogenerate: { directory: "网络安全" },
+          collapsed: true,
+        },
+        {
+          label: "开发",
+          autogenerate: { directory: "开发" },
+          collapsed: true,
+        },
+        {
+          label: "容器化",
+          autogenerate: { directory: "容器化" },
+          collapsed: true,
+        },
+        {
+          label: "小作文",
+          autogenerate: { directory: "小作文" },
+          collapsed: true,
+        },
+      ],
+      locales: {
+        root: {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });

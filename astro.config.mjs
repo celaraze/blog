6cdb598ff0bb44288ff66f6286658a064e01a5a8
celaraze <celaraze@qq.com>
2024-05-29 17:22:40 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
     outDir: './docs',
-    base: '/docs',
     integrations: [
         starlight({
             title: "Celaraze Blog",

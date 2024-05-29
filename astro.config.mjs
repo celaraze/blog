41dbefa4f7ada3c033df0ac9e2c64994bd72b63a
celaraze <celaraze@qq.com>
2024-05-29 17:00:01 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,40 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celaraze.com",
-  integrations: [
-    starlight({
-      title: "Celaraze Blog",
-      social: {
-        github: "https://github.com/celaraze",
-      },
-      sidebar: [
-        {
-          label: "了解我",
-          link: "/",
-        },
-        {
-          label: "文章列表",
-          autogenerate: { directory: "articles" },
-          collapsed: true,
-          badge: { text: "建议使用搜索", variant: "success" },
-        },
-        {
-          label: "CAT 文档",
-          autogenerate: { directory: "projects/cat" },
-          collapsed: true,
-        },
-      ],
-      locales: {
-        root: {
-          label: "简体中文",
-          lang: "zh-CN",
-        },
-      },
-      customCss: ["./src/styles/custom.css"],
-    }),
-  ],
+    site: "https://celaraze.github.io",
+    base: "blog",
+    integrations: [
+        starlight({
+            title: "Celaraze Blog",
+            social: {
+                github: "https://github.com/celaraze",
+            },
+            sidebar: [
+                {
+                    label: "了解我",
+                    link: "/",
+                },
+                {
+                    label: "文章列表",
+                    autogenerate: {directory: "articles"},
+                    collapsed: true,
+                    badge: {text: "建议使用搜索", variant: "success"},
+                },
+                {
+                    label: "CAT 文档",
+                    autogenerate: {directory: "projects/cat"},
+                    collapsed: true,
+                },
+            ],
+            locales: {
+                root: {
+                    label: "简体中文",
+                    lang: "zh-CN",
+                },
+            },
+            customCss: ["./src/styles/custom.css"],
+        }),
+    ],
 });
